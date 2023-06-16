f8ee1e98f0e506528400578e292c1bd2458e0129
Hoang Minh <trinhminhhoang3012@gmail.com>
2023-06-16 16:28:18 +0700

    update ui

--- OTA.ino
+++ OTA.ino
@@ -37,6 +37,7 @@ const String STM32_CHIPNAME[8] = {
   "Unknown Chip",
   "STM32F103x8/B",
 };
+String html;
 File fsUploadFile;
 uint8_t binread[256];
 int bini = 0;
@@ -44,6 +45,7 @@ String stringtmp;
 int rdtmp = 0;
 bool Runflag = 0;
 bool mcu = 1;
+
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
@@ -122,8 +124,7 @@ void handleFlash(AsyncWebServerRequest *request)
       }
     //flashwr += String(binread[0]) + "," + String(binread[lastbuf - 1]) + "<br>";
     fsUploadFile.close();
-    String flashhtml = "<h1>Programming</h1><h2>" + flashwr +  "<br><br><a style=\"color:white\" href=\"/\">Back home to RUN MCU</a></h2>";
-    request->send(200, "text/html", makePage("Flash Page", flashhtml));
+    request->send(200, "text/plain", " Programming done");
   }
 }
 
@@ -131,7 +132,6 @@ void handleUpload(AsyncWebServerRequest *request, String filename, size_t index,
   if(!index){
     if (!filename.startsWith("/")) filename = "/" + filename;
     fsUploadFile = SPIFFS.open(filename, "w");
-    filename = String();
   }
   if(len) {
     if (fsUploadFile)
@@ -155,39 +155,35 @@ void handleFileDelete(AsyncWebServerRequest *request) {
   FileList += FName;
   FName = "/" + FName;
   if (SPIFFS.exists(FName)) {
-    request->send(200, "text/html", makePage("Deleted", "<h2>" + FileList + " be deleted!<br><br><a style=\"color:white\" href=\"/\">Return </a></h2>"));
     SPIFFS.remove(FName);
   }
-  else{
-    return request->send(404, "text/html", makePage("File Not found", "<h2>File Not found<br><br><a style=\"color:white\" href=\"/\">Return </a></h2>"));
-  }
+  request->redirect("/list");
 }
 
 String handleListFiles()
 {
   String FileList;
   String Listcode;
-  FileList += " File: ";
   File root = SPIFFS.open("/");
   File file = root.openNextFile();
   while (file)
   {
     String FileName = file.name();
-    File f = SPIFFS.open("/"+FileName,"r");
-    String FileSize = String(f.size());
+    file = SPIFFS.open("/bin"+FileName,"r");
+    String FileSize = String(file.size());
     int whsp = 6 - FileSize.length();
     while (whsp-- > 0)
     {
       FileList += " ";
     }
-    FileList +=  FileName + "   Size: " + FileSize + " bytes <br>";
+    FileList +=  FileName + "-" + FileSize + " bytes <br>";
     file = root.openNextFile();
   }
-  Listcode = "<h2>List STM32 BinFile</h2><h2>" + FileList + "<br><br><a style=\"color:white\" href=\"/delete\">Delete BinFile </a>";
-  return Listcode;
+  FileList += "<a href=\"delete\"> DELETE FILE </a><br>";
+  return FileList;
 }
 
-void initMCU (){
+bool initMCU (){
   FlashMode();
   if (mcu) digitalWrite(NRST2, LOW);
   else digitalWrite(NRST1, LOW);
@@ -200,6 +196,7 @@ void initMCU (){
   rdtmp = Serial.read();    
   if (rdtmp == STM32ACK) {
     stringtmp = STM32_CHIPNAME[stm32GetId()];
+    return true;
   }
   else if (rdtmp == STM32NACK) {
     Serial.write(STM32INIT);
@@ -208,14 +205,17 @@ void initMCU (){
     delay(10);
     if (rdtmp == STM32ACK){
       stringtmp = STM32_CHIPNAME[stm32GetId()];
+      return true;
     }
+    return 0;
   }
   else{
     stringtmp = "ERROR";
+    return 0;
   }
 }
 void FlashMode()  {    //Tested  Change to flashmode
-  if (Runflag == 1) {
+  //if (Runflag == 1) {
     digitalWrite(BOOT01, HIGH);
     digitalWrite(BOOT02, HIGH);
     delay(100);
@@ -226,11 +226,11 @@ void FlashMode()  {    //Tested  Change to flashmode
     digitalWrite(NRST2, HIGH);
     delay(10);
     Runflag = 0;
-  }
+  //}
 }
 
 void RunMode()  {    //Tested  Change to runmode
-  if(Runflag==0){
+  //if(Runflag==0){
     digitalWrite(BOOT01, LOW);
     digitalWrite(BOOT02, LOW);
     delay(100);
@@ -241,7 +241,7 @@ void RunMode()  {    //Tested  Change to runmode
     digitalWrite(NRST2, HIGH);
     delay(200);
     Runflag=1;
-  }
+  //}
 }
 // Initialize SPIFFS
 void initSPIFFS() {
@@ -285,22 +285,18 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
   }
 }
 // Initialize WiFi
-bool initWiFi() {
-  if(ssid=="" || ip==""){
-    Serial.println("Undefined SSID or IP address.");
-    return false;
-  }
-  WiFi.mode(WIFI_STA);
+void initWiFi() {
+  WiFi.mode(WIFI_MODE_APSTA);
+  WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+  
   localIP.fromString(ip.c_str());
   localGateway.fromString(gateway.c_str());
 
-
   if (!WiFi.config(localIP, localGateway, subnet)){
     Serial.println("STA Failed to configure");
-    return false;
   }
+
   WiFi.begin(ssid.c_str(), pass.c_str());
-  Serial.println("Connecting to WiFi...");
 
   unsigned long currentMillis = millis();
   previousMillis = currentMillis;
@@ -309,11 +305,10 @@ bool initWiFi() {
     currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
       Serial.println("Failed to connect.");
-      return false;
+      break;
     }
   }
   Serial.println(WiFi.localIP());
-  return true;
 }
 
 void setup() {
@@ -324,7 +319,6 @@ void setup() {
   pinMode(BOOT02, OUTPUT);
   pinMode(NRST2, OUTPUT);
   initSPIFFS();	
-  WiFi.mode(WIFI_MODE_APSTA);
   // Load values saved in SPIFFS
   ssid = readFile(SPIFFS, ssidPath);
   pass = readFile(SPIFFS, passPath);
@@ -334,125 +328,116 @@ void setup() {
   Serial.println(pass);
   Serial.println(ip);
   Serial.println(gateway);
+  //init wifi
+  initWiFi(); 
+  // OTA route
+  server.serveStatic("/", SPIFFS, "/");
+  server.on("/programm", HTTP_GET, handleFlash);
+  server.on("/run1", HTTP_GET, [](AsyncWebServerRequest *request) {
+    RunMode();
+    digitalWrite(NRST2, LOW);
+    request->send(SPIFFS, "/run1.html", "text/html");
+  });
+  server.on("/run2", HTTP_GET, [](AsyncWebServerRequest *request) {
+    RunMode();
+    digitalWrite(NRST1, LOW);
+    request->send(SPIFFS, "/run2.html", "text/html");
+  });
+  server.on("/erase", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (stm32Erase() == STM32ACK)
+      request->send(200, "text/plain", "Erase done");
+    else if (stm32Erasen() == STM32ACK)
+      request->send(200, "text/plain", "Erase done");
+    else
+      request->send(200, "text/plain", "Erase fail");
+  });
+  server.on("/delete", HTTP_GET, [](AsyncWebServerRequest *request){
+    handleFileDelete(request);
+  });
 
-
-  if(initWiFi()) {
-    // OTA route
-    server.on("/programm", HTTP_GET, handleFlash);
-    server.on("/run", HTTP_GET, [](AsyncWebServerRequest *request) {
-      String Runstate ;
-      Runstate += "<h2>STM32 Restart and runing!<br><br></h2>";
-      if (Runflag == 0) {
-        RunMode();
-        Runflag = 1;
-      }
-      request->send(200, "text/html", makePage("Run",  Runstate + "<h2><a style=\"color:white\" href=\"/\"> Home </a></h2>"));
-    });
-    server.on("/erase", HTTP_GET, [](AsyncWebServerRequest *request) {
-      if (stm32Erase() == STM32ACK)
-        stringtmp = "<h1>Erase OK</h1><h2><a style=\"color:white\" href=\"/\">Return </a></h2>";
-      else if (stm32Erasen() == STM32ACK)
-        stringtmp = "<h1>Erase OK</h1><h2><a style=\"color:white\" href=\"/\">Return </a></h2>";
-      else
-        stringtmp = "<h1>Erase failure</h1><h2><a style=\"color:white\" href=\"/\">Return </a></h2>";
-      request->send(200, "text/html", makePage("Erase page", stringtmp));
-    });
-    server.on("/delete", HTTP_GET, [](AsyncWebServerRequest *request){
-      handleFileDelete(request);
-    });
-
-    server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/html", makePage("FileList", "<h1> Uploaded OK </h1><br><br><h2><a style=\"color:white\" href=\"/\">Return </a></h2>"));
-    }, handleUpload);
-    server.on("/stm1", HTTP_GET ,[](AsyncWebServerRequest *request){
-      mcu = 1;
-      initMCU();
-      String starthtml = "<h1><a style=\"color:white\" href=\"/\">BACK HOME</a><h1>BOOT STATE</h1><h1>1st MCU</h1></h1><h2><br><a style=\"color:white\" href=\"/programm\">Programm MCU</a><br><br><a style=\"color:white\" href=\"/erase\">Erase MCU</a></h2>";
-      request->send(200,"text/html",makePage("MCU 1",starthtml + "- Init MCU -<br> " + stringtmp));
-    });
-
-    server.on("/stm2",HTTP_GET,[](AsyncWebServerRequest *request){
-      mcu = 0;
-      initMCU();
-      String starthtml = "<h1><a style=\"color:white\" href=\"/\">BACK HOME</a><h1>BOOT STATE</h1><h1>2nd MCU</h1></h1><h2><br><a style=\"color:white\" href=\"/programm\">Programm MCU</a><br><br><a style=\"color:white\" href=\"/erase\">Erase MCU</a></h2>";
-      request->send(200,"text/html",makePage("MCU 2",starthtml + "- Init MCU -<br> " + stringtmp));
-    });
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-      RunMode();
-      String starthtml = "<h1>STM32-OTA</h1><h1>RUN STATE</h1><h2><a style=\"color:white\" href=\"/stm1\">1.First STM32</a><br><br><a style=\"color:white\" href=\"/stm2\">2.Second STM32</a></h2><br><h2>Upload STM32 BinFile</h2><h2><form method='POST' action='/upload' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Upload'></form></h2>";
-      String temp = handleListFiles();
-      starthtml += temp;
-      request->send(200,"text/html",makePage("MCU 2",starthtml + "- Init MCU -<br> " + stringtmp));
-    });
-    server.serveStatic("/", SPIFFS, "/");
-    
-    server.begin();
-  }
-  else {
-    // Connect to Wi-Fi network with SSID and password
-    Serial.println("Setting AP (Access Point)");
-    // NULL sets an open Access Point
-    WiFi.softAP("ESP-WIFI-MANAGER", NULL);
-
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP); 
-
-    // Web Server Root URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SPIFFS, "/wifimanager.html", "text/html");
-    });
-    
-    server.serveStatic("/", SPIFFS, "/");
-    
-    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
-      int params = request->params();
-      for(int i=0;i<params;i++){
-        AsyncWebParameter* p = request->getParam(i);
-        if(p->isPost()){
-          // HTTP POST ssid value
-          if (p->name() == PARAM_INPUT_1) {
-            ssid = p->value().c_str();
-            Serial.print("SSID set to: ");
-            Serial.println(ssid);
-            // Write file to save value
-            writeFile(SPIFFS, ssidPath, ssid.c_str());
-          }
-          // HTTP POST pass value
-          if (p->name() == PARAM_INPUT_2) {
-            pass = p->value().c_str();
-            Serial.print("Password set to: ");
-            Serial.println(pass);
-            // Write file to save value
-            writeFile(SPIFFS, passPath, pass.c_str());
-          }
-          // HTTP POST ip value
-          if (p->name() == PARAM_INPUT_3) {
-            ip = p->value().c_str();
-            Serial.print("IP Address set to: ");
-            Serial.println(ip);
-            // Write file to save value
-            writeFile(SPIFFS, ipPath, ip.c_str());
-          }
-          // HTTP POST gateway value
-          if (p->name() == PARAM_INPUT_4) {
-            gateway = p->value().c_str();
-            Serial.print("Gateway set to: ");
-            Serial.println(gateway);
-            // Write file to save value
-            writeFile(SPIFFS, gatewayPath, gateway.c_str());
-          }
-          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request) {
+  request->send(200, "text/plain", "Upload done");
+  }, handleUpload);
+  server.on("/list", HTTP_GET ,[](AsyncWebServerRequest *request){
+    String listname =  handleListFiles();
+    request->send(200, "text/html", listname);
+  });
+  server.on("/stm1", HTTP_GET ,[](AsyncWebServerRequest *request){
+    mcu = 1;
+    initMCU();
+    request->send(SPIFFS, "/boot1.html", "text/html");
+  });
+  server.on("/stm2",HTTP_GET,[](AsyncWebServerRequest *request){
+    mcu = 0;
+    initMCU();
+    request->send(SPIFFS, "/boot2.html", "text/html");
+  });
+  server.on("/ota", HTTP_GET, [](AsyncWebServerRequest *request) {
+    RunMode();
+    request->send(SPIFFS, "/ota.html", "text/html");
+  });
+  server.on("/about", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/about.html", "text/html");
+  });
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP); 
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/wifimanager.html", "text/html");
+  });
+  
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST ssid value
+        if (p->name() == PARAM_INPUT_1) {
+          ssid = p->value().c_str();
+          Serial.print("SSID set to: ");
+          Serial.println(ssid);
+          // Write file to save value
+          writeFile(SPIFFS, ssidPath, ssid.c_str());
+        }
+        // HTTP POST pass value
+        if (p->name() == PARAM_INPUT_2) {
+          pass = p->value().c_str();
+          Serial.print("Password set to: ");
+          Serial.println(pass);
+          // Write file to save value
+          writeFile(SPIFFS, passPath, pass.c_str());
+        }
+        // HTTP POST ip value
+        if (p->name() == PARAM_INPUT_3) {
+          ip = p->value().c_str();
+          Serial.print("IP Address set to: ");
+          Serial.println(ip);
+          // Write file to save value
+          writeFile(SPIFFS, ipPath, ip.c_str());
+        }
+        // HTTP POST gateway value
+        if (p->name() == PARAM_INPUT_4) {
+          gateway = p->value().c_str();
+          Serial.print("Gateway set to: ");
+          Serial.println(gateway);
+          // Write file to save value
+          writeFile(SPIFFS, gatewayPath, gateway.c_str());
         }
       }
-      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
-      delay(2000);
-      ESP.restart();
-    });
-    server.begin();
-  }
+    }
+    request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+    delay(2000);
+    ESP.restart();
+  });
+  server.begin();
 }
-
+/*
+String updateMode(String MCU1,String MCU2){
+  html = "<!DOCTYPE html><html><head><title>OTA</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/png\" href=\"favicon.png\"><link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\"></head><body><div class=\"topnav\">";
+  html+="<a href=\"/\"><h1>Wifi Config</h1></a><a href=\"/ota\"><h1>Flash MCU</h1></a><a href=\"/about\"><h1>About</h1></a></div><div class=\"content\"><p>1.First STM32</p><p><a href=\"/run1\">Run</a></p><p><a href=\"/stm1\">Boot</a></p><p><a href=\"/programm\">Programm</a></p><p><a href=\"/erase\">Erase</a></p><p>2.Second STM32<br><p><a href=\"/run2\">Run</a></p><p><a href=\"/stm2\">Boot</a></p><p><a href=\"/programm\">Programm</a></p><p><a href=\"/erase\">Erase</a></p>";
+  html+="<p>Upload STM32 BinFile</p><p><form method='POST' action='/upload' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Upload'></form></p></div></body></html>"; 
+}
 String makePage(String title, String contents){
   String s = "<!DOCTYPE html><html><head>";
   s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
@@ -467,6 +452,7 @@ String makePage(String title, String contents){
   s += "</body></html>";
   return s;
 }
+*/
 void loop() {
   //server.handleClient();
 }
